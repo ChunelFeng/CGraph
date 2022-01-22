29a7818bd8602a3e22f37252c66e656026395f31
ChunelFeng <chunel@foxmail.com>
2022-01-22 21:38:12 +0800

    [chg] 优化线程池功能

---
+++ tutorial/MyGAspect/MyPipelineParamAspect.h
@@ -0,0 +1,44 @@
+/***************************
+@Author: Chunel
+@Contact: chunel@foxmail.com
+@File: MyPipelineParamAspect.h
+@Time: 2021/10/5 11:48 上午
+@Desc: 
+***************************/
+
+#ifndef CGRAPH_MYPIPELINEPARAMASPECT_H
+#define CGRAPH_MYPIPELINEPARAMASPECT_H
+
+#include "../../src/CGraph.h"
+#include "../MyGParam/MyParam.h"
+
+class MyPipelineParamAspect : public CGraph::GAspect {
+public:
+    CStatus beginRun() override {
+        CStatus status;
+
+        /**
+         * 在切面层，获取pipeline中的参数值信息，进行一些逻辑处理
+         * 可以用于异常判断、限流等逻辑
+         * */
+        auto* pipelineParam = this->getPipelineParam<MyParam>("param1");
+        if (nullptr == pipelineParam) {
+            return CStatus("pipelineParam is null");
+        }
+
+        /** 遇到并发情况，请考虑加锁保护（参考 T05-Param 中逻辑） */
+        int cnt = pipelineParam->iCount;
+        CGraph::CGRAPH_ECHO("----> [MyPipelineParamAspect] pipeline param iCount is [%d] before run.", cnt);
+
+        if (cnt < 0) {
+            /** 模拟：在切面中，对pipeline中的参数，做一些异常值的处理逻辑
+             * 在 beginRun() 切面中 返回非STATUS_OK值，则pipeline停止执行
+             * */
+            return CStatus("aspect demo error");
+        }
+
+        return status;
+    }
+};
+
+#endif //CGRAPH_MYPIPELINEPARAMASPECT_H
