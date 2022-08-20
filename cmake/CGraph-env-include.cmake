83f9cf3fb37a7cfa808539b4c0f37b5ba52459dc
ChunelFeng <Chunel@foxmail.com>
2022-08-21 01:25:14 +0800

    [chg] 优化了对windows系统的支持情况

--- cmake/CGraph-env-include.cmake
+++ cmake/CGraph-env-include.cmake
@@ -7,7 +7,14 @@ IF(APPLE)
     add_definitions(-D_GENERATE_SESSION_)
     add_definitions(-D_ENABLE_LIKELY_)
 ELSEIF(UNIX)
-    # linux平台加入多线程内容
+    # linux平台，加入多线程内容
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -pthread -Wno-format-overflow")
     add_definitions(-D_ENABLE_LIKELY_)
+ELSEIF(WIN32)
+    # windows平台，加入utf-8设置。否则无法通过编译
+    add_definitions(/utf-8)
+
+    # 禁止两处warning级别提示
+    add_compile_options(/wd4996)
+    add_compile_options(/wd4267)
 ENDIF()
