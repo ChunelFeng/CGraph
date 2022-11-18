0a153db152165388ef0f3098be5293b2b4e143a7
ChunelFeng <chunel@foxmail.com>
2022-11-19 00:36:34 +0800

    [chg] 更新一处依赖关系逻辑

--- src/GraphCtrl/GraphPipeline/GPipelineFactory.h
+++ src/GraphCtrl/GraphPipeline/GPipelineFactory.h
@@ -13,11 +13,11 @@
 #include <mutex>
 
 #include "GPipeline.h"
-#include "../../UtilsCtrl/UtilsInclude.h"
+#include "../GraphObject.h"
 
 CGRAPH_NAMESPACE_BEGIN
 
-class GPipelineFactory : public CObject {
+class GPipelineFactory : public GraphObject {
 
 public:
     /**
