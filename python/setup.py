9d497b6230eaae1580d04ec0424420c929e4d431
Chunel <Chunel@foxmail.com>
2025-11-08 17:38:55 +0800

    [tag] v3.2.2

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, Extension
 import pybind11
 
 __PYCGRAPH_NAME__ = "pycgraph"
-__PYCGRAPH_VERSION__ = "3.2.1"
+__PYCGRAPH_VERSION__ = "3.2.2"
 __PYCGRAPH_AUTHOR__ = "Chunel"
 __PYCGRAPH_AUTHOR_EMAIL__ = "chunel@foxmail.com"
 __PYCGRAPH_DESCRIPTION__ = "CGraph with python api wrapper by pybind11"
