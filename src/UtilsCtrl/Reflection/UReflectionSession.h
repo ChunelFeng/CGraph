0c2fbf6b0ea62ff0eacce5bf47b947677d28db9d
Chunel <Chunel@foxmail.com>
2026-03-24 23:43:17 +0800

    [chron] fix bazel compile warning

--- src/UtilsCtrl/Reflection/UReflectionSession.h
+++ src/UtilsCtrl/Reflection/UReflectionSession.h
@@ -16,67 +16,67 @@ CGRAPH_INTERNAL_NAMESPACE_BEGIN
 
 class UReflSession : public UtilsObject {
 public:
-    explicit UReflSession(CUCharPtr data, size_t len)
-            : data_(data), len_(len) {}
+    explicit UReflSession(const CUCharPtr data, const size_t len)
+            : len_(len), data_(data) {}
 
     explicit UReflSession() = default;
 
     template<class T>
-    inline void getValueAndOffset(T &value) {
+    void getValueAndOffset(T &value) {
         constexpr size_t len = sizeof(T);
         memcpy(&value, data_ + offset_, len);
         offset_ += len;
     }
 
-    inline void setBufferAndOffset(CUCharPtr buffer, size_t len) {
+    void setBufferAndOffset(CUCharPtr buffer, const size_t len) {
         memcpy(data_ + offset_, buffer, len);
         offset_ += len;
     }
 
-    inline void getStringAndOffset(std::string &value, size_t len) {
+    void getStringAndOffset(std::string &value, const size_t len) {
         value.assign((CCharPtr)(data_ + offset_), len);
         offset_ += len;
     }
 
     template<class T>
-    inline void setValueAndOffset(const T &value) {
+    void setValueAndOffset(const T &value) {
         constexpr size_t len = sizeof(T);
         memcpy(data_ + offset_, &value, len);
         offset_ += len;
     }
 
-    inline void addLen(size_t len) {
+    void addLen(const size_t len) {
         len_ += len;
     }
 
-    inline void addOffset(size_t len) {
+    void addOffset(const size_t len) {
         offset_ += len;
     }
 
-    inline void setPosValue(size_t offset, CUCharPtr value, size_t len) const {
+    void setPosValue(const size_t offset, CUCharPtr value, const size_t len) const {
         memcpy(data_ + offset, value, len);
     }
 
-    [[nodiscard]] inline CUCharPtr getCurrentPos() const {
+    [[nodiscard]] CUCharPtr getCurrentPos() const {
         return data_ + offset_;
     }
 
-    [[nodiscard]] inline size_t getCurrentOffset() const {
+    [[nodiscard]] size_t getCurrentOffset() const {
         return offset_;
     }
 
-    [[nodiscard]] inline bool isEmpty() const {
+    [[nodiscard]] bool isEmpty() const {
         return len_ <= offset_;
     }
 
-    [[nodiscard]] inline size_t getLen() const {
+    [[nodiscard]] size_t getLen() const {
         return len_;
     }
 
 private:
-    size_t len_{0};                         // 整体长度
-    size_t offset_{0};                      // 当前偏移量
-    CUCharPtr data_{nullptr};               // 真实数据
+    size_t len_ {0};                         // 整体长度
+    size_t offset_ {0};                      // 当前偏移量
+    CUCharPtr data_ {nullptr};               // 真实数据
 };
 
 CGRAPH_INTERNAL_NAMESPACE_END
